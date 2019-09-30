11a1878a6853b27dd767ee78a138222bfbad5a98
Shivang Sharma <45060274+shivang1305@users.noreply.github.com>
2019-10-01 01:22:25 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+int main(int argc, char *argv[])
+{
+	printf("*************WELCOME**************");
+    // idk what this, is just go along
+    memset(&app, 0, sizeof(App));
+    memset(&player, 0, sizeof(Entity));
+
+    player.rect.x = 50;
+    player.rect.y = 10;
+    player.rect.h = 100;
+    player.rect.w = 20;
+    player.r = 255;
+    player.g = 255;
+    player.b = 255;
+
+    initSDL();       // Intialise
+    atexit(cleanup); // Destroy stuff
+
+    while (1)
+    {
+        prepareScene();
+        doInput();
+
+        if (app.up && (player.rect.y) >= 0)
+        {
+            player.rect.y -= 10;
+        }
+        if (app.down && (player.rect.y + 100) <= SCREEN_HEIGHT)
+        {
+            player.rect.y += 10;
+        }
+
+        drawEntity(player);
+        presentScene();
+        SDL_Delay(16);
+    }
+    return 0;
+}
